7a6945f70d6928d11b00ec753126ec7c6443eb79
LamVu1 <lvu2548@gmail.com>
2020-01-29 16:14:37 -0800

    Styled landing and navigation bar

--- frontend/pages/landing.jsx
+++ frontend/pages/landing.jsx
@@ -2,12 +2,54 @@ import React from 'react';
 import SignIn from '../components/sign_in_component';
 import SignUp from '../components/sign_up_component';
 
-const LandingPage = ()=>(
-    <div className='sign-in-and-sign-up'>
-       <h1>Land</h1>
-       <SignIn />
-       <SignUp />
-    </div>
-)
+class LandingPage extends React.Component{
+    constructor(){
+        super();
+        this.flag = true;
+        this.handleButton = this.handleButton.bind(this);
+    }
+
+    handleButton(){
+        let signin = document.getElementsByClassName('sign-in')
+        let signup = document.getElementsByClassName('sign-up')
+        let btn = document.getElementsByClassName("form-btn")
+        if(this.flag){
+            signin[0].style.display = "none";
+            signup[0].style.display = "flex";
+            btn[0].innerHTML = "Have an account already? Sign In"
+            this.flag = false;
+        }
+        else{
+            signin[0].style.display = "flex";
+            signup[0].style.display = "none";
+            btn[0].innerHTML = " New to myshop? Sign Up"
+            this.flag = true;
+
+        }
+    }
+
+    render(){
+
+        let btn;
+        
+            btn =  <div className="form-btn" onClick={this.handleButton}>
+            New to myshop? Sign Up
+        </div>
+        
+
+        return(
+            <div className='landing-page'>
+                <div className='form-div'>
+                    <SignIn />
+                    <SignUp />
+                    {btn}
+                </div>
+            </div>
+        )
+    }
+
+
+
+}
 
 export default LandingPage;
