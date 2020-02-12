9d1b3d903a737c51036d5f880e1aa5c469d464df
LamVu1 <lvu2548@gmail.com>
2020-02-11 23:36:17 -0800

    Setup activeStorage and AWS

--- frontend/components/footer_component.jsx
+++ frontend/components/footer_component.jsx
@@ -5,13 +5,13 @@ const Footer = ()=>{
     return(
         <div className='Footer'>
             <div className='links'>
-                <a href="https://www.linkedin.com/in/lam-vu-4b49a5117/" target="_blank" >
+                <a href="https://github.com/LamVu1/EcommerceProject" target="_blank" >
                     <img className='icon' src={window.github}/>
                 </a>
                 <a href="https://www.linkedin.com/in/lam-vu-4b49a5117/" target="_blank" >
                     <img className='icon' src={window.linkedin}/>
                 </a> 
-                <a href="https://www.linkedin.com/in/lam-vu-4b49a5117/" target="_blank" >
+                <a href="https://angel.co/lam-vu-2" target="_blank" >
                     <img className='icon' src={window.angelist}/>
                 </a>
             </div>
