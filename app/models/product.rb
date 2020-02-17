1db4779ca7df053cab9ff710121b99f16b50721b
LamVu1 <lvu2548@gmail.com>
2020-02-17 12:08:47 -0800

    Debug Heroku

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ApplicationRecord
     validates :title, :description, :price,  presence: true
-
+    
     has_many_attached :photos
+
+    def ensure_photo
+        if !self.photos.attached?
+            self.photos.attach(io: File.open('app/assets/images/logo.png'), filename: 'logo.png', content_type: 'image/png')
+        end
+    end
+
 
     has_many :cart_items,
     class_name: :CartItem,
